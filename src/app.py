63b766aa482568729628b69b28ab9923eefeff16
Ibusteros <ibusterosr193@gmail.com>
2023-03-17 12:00:42 +0100

    cambios

--- src/app.py
+++ src/app.py
@@ -1,23 +1,28 @@
 from api_handler import BMEApiHandler
 from eq_w import EqwAlgo
 
+#Ctrl K C
+#Ctrl K U
+
 
 def handler(event, context):
     # Llamar al algo
-    try:
-        eq_w_1 = EqwAlgo(algo_tag='ibusteros_algo1', n_days=10)
-        eq_w_1.run_day_comp()
-    except Exception as e:
-        print(f"Error en algo 1 {e}")
+    # try:
+    #     eq_w_1 = EqwAlgo(algo_tag='ibusteros_algo1', n_days=10)
+    #     eq_w_1.run_day_comp()
+    # except Exception as e:
+    #     print(f"Error en algo 1 {e}")
 
-    try:
-        eq_w_2 = EqwAlgo(algo_tag='ibusteros_algo1', n_days=20)
-        eq_w_2.run_day_comp()
-    except Exception as e:
-        print(f"Error en algo 2 {e}")
+    # try:
+    #     eq_w_2 = EqwAlgo(algo_tag='ibusteros_algo1', n_days=20)
+    #     eq_w_2.run_day_comp()
+    # except Exception as e:
+    #     print(f"Error en algo 2 {e}")
 
-    try:
-        eq_w_3 = EqwAlgo(algo_tag='ibusteros_algo1', n_days=30)
-        eq_w_3.run_day_comp()
-    except Exception as e:
-        print(f"Error en algo 3 {e}")
+    # try:
+    #     eq_w_3 = EqwAlgo(algo_tag='ibusteros_algo1', n_days=30)
+    #     eq_w_3.run_day_comp()
+    # except Exception as e:
+    #     print(f"Error en algo 3 {e}")
+
+    print("HOLA")
